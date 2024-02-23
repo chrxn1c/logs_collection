7a481dc53d0167692286fa7e4878edafeab070dc
chrxn1c <rivalofficialentertainment@gmail.com>
2024-02-23 23:40:14 +0300

    chore: user class is now hashable

--- user.py
+++ user.py
@@ -7,8 +7,11 @@ class User:
         User.user_count += 1
         self.id: int = User.user_count
 
+    def get_full_name(self) -> str:
+        return f"{self.first_name} {self.second_name}"
+
     def __str__(self) -> str:
         return f"{self.first_name} {self.second_name}, id = {self.id}"
 
-    def get_full_name(self) -> str:
-        return f"{self.first_name} {self.second_name}"
+    def __hash__(self) -> str:
+        return hash(repr(self))
