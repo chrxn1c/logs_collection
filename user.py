376eb89c2ba0d065776b34e83409f0562cec4466
chrxn1c <rivalofficialentertainment@gmail.com>
2024-02-23 23:33:31 +0300

    fix: addressing static field is working

--- user.py
+++ user.py
@@ -4,8 +4,8 @@ class User:
     def __init__(self, first_name: str, second_name: str) -> None:
         self.first_name: str = first_name
         self.second_name: str = second_name
-        user_count += 1
-        self.id: int = user_count
+        User.user_count += 1
+        self.id: int = User.user_count
 
     def __str__(self) -> str:
         return f"{self.first_name} {self.second_name}, id = {self.id}"
