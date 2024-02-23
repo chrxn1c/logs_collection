2751f22d79b74fef1b4375735cf253e712953d44
chrxn1c <rivalofficialentertainment@gmail.com>
2024-02-23 23:04:27 +0300

    users' id don't have to be passed

--- user.py
+++ user.py
@@ -1,8 +1,11 @@
 class User:
-    def __init__(self, first_name: str, second_name: str, id: int) -> None:
+    user_count: int = 0
+
+    def __init__(self, first_name: str, second_name: str) -> None:
         self.first_name: str = first_name
         self.second_name: str = second_name
-        self.id: int = id
+        user_count += 1
+        self.id: int = user_count
 
     def __str__(self) -> str:
         return f"{self.first_name} {self.second_name}, id = {self.id}"
