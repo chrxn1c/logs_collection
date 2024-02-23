9d71f397dd5425ac2e5c44487c09dbefbc9f1896
chrxn1c <rivalofficialentertainment@gmail.com>
2024-02-24 00:25:45 +0300

    refactor: logs are now stored inside Logger class

--- logger.py
+++ logger.py
@@ -1,14 +1,13 @@
 from log import Log, LoggingLevel
 from user import User
 
-logs: list[Log] = []
-
 
 class Logger:
     def __init__(self, trackable_users: list[User]) -> None:
         self.tracked_users: list[User] = trackable_users.copy()
         self.tracked_users_to_events_count: dict[str:int] = {}
         self._initialize_new_tracked_users_to_dict(self.tracked_users)
+        self._logs: list[Log] = []
 
     def track_new_users(self, new_tracked_users: list[User]) -> None:
         for new_tracked_user in new_tracked_users:
@@ -17,7 +16,7 @@ class Logger:
         self._initialize_new_tracked_users_to_dict(new_tracked_users)
 
     def accept_event(self, log: Log) -> None:
-        logs.append(log)
+        self._logs.append(log)
 
         if not (log.user in self.tracked_users):
             return
@@ -28,14 +27,14 @@ class Logger:
 
     def retrieve_by_logging_level(self, requested_level: LoggingLevel) -> list[Log]:
         fetched_logs: list[Log] = []
-        for log in logs:
+        for log in self._logs:
             if requested_level == log.logging_level:
                 fetched_logs.append(log)
         return fetched_logs
 
     def output_logs(self) -> None:
         with open("logs.txt", "w+") as outputted_logs:
-            for log in logs:
+            for log in self._logs:
                 outputted_logs.write(str(log) + "\n")
 
     def _initialize_new_tracked_users_to_dict(self, new_tracked_users: list[User]) -> None:
